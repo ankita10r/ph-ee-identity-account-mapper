e149518892ab880594c499187aa2e5ce797f5dab
Ankita Ranjan <ankita@fynarfin.io>
2023-08-04 18:54:40 +0530

    gov556

--- src/main/java/org/mifos/identityaccountmapper/api/definition/UpdatePaymentModalityApi.java
+++ src/main/java/org/mifos/identityaccountmapper/api/definition/UpdatePaymentModalityApi.java
@@ -1,6 +1,8 @@
 package org.mifos.identityaccountmapper.api.definition;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.mifos.identityaccountmapper.data.RequestDTO;
 import org.mifos.identityaccountmapper.data.ResponseDTO;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.concurrent.ExecutionException;
-
+@Tag(name = "GOV")
 public interface UpdatePaymentModalityApi {
     @PutMapping("/paymentModality")
-    ResponseEntity<ResponseDTO> updatePaymentModality(@RequestHeader(value="X-CallbackURL") String callbackURL, @RequestHeader(value = "registeringInstitutionId") String registeringInstitutionId,
+    @Operation(
+            summary = "Updating payment modality")
+    ResponseEntity<ResponseDTO> updatePaymentModality(@RequestHeader(value="X-CallbackURL") String callbackURL,
+                                                      @RequestHeader(value = "registeringInstitutionId") String registeringInstitutionId,
                                                       @RequestBody RequestDTO requestBody) throws ExecutionException, InterruptedException, JsonProcessingException;
 
 }
